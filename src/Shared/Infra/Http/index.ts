bdc5f6372d55c3491f894eb86339f24e3ce076e7
Daniel Oliveira <oficial.danieloliveira@gmail.com>
2020-12-08 00:24:25 -0300

    feat(database): set up database :rocket:

--- src/Shared/Infra/Http/index.ts
+++ src/Shared/Infra/Http/index.ts
@@ -1,7 +1,13 @@
 import express from 'express';
 
-const app = express();
+import TypeORM from '../TypeORM';
 
-app.listen(3000, () => {
-  console.log('Working!');
+const app = express();
+const port = process.env.APP_PORT || 3333;
+const typeORM = new TypeORM();
+
+typeORM.run();
+
+app.listen(port, () => {
+  console.log(`\x1b[33mServer Running On Port ${port}\x1b[0m`);
 });
