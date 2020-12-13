3542bd86b99e2e42b6bba4810ace892137eeea4b
Daniel Oliveira <oficial.danieloliveira@gmail.com>
2020-12-13 16:06:42 -0300

    feat(currencies): set up currencies routes temporally to make updates manually :moneybag:

---
+++ src/Modules/Currencies/Infra/Http/Routes/Currencies.routes.ts
@@ -0,0 +1,15 @@
+import { Router } from 'express';
+
+import AuthController from 'Shared/Containers/AuthProvider/Controllers/AuthController';
+import CurrenciesController from '../Controllers/CurrenciesController';
+
+const currenciesRoutes = Router();
+
+const authController = new AuthController();
+const currenciesController = new CurrenciesController();
+
+currenciesRoutes.post('/', authController.on, currenciesController.create);
+
+currenciesRoutes.patch('/', authController.on, currenciesController.update);
+
+export default currenciesRoutes;
