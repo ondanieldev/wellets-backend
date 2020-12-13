603336827c192afbd36452e1ce8135f24bb5c817
Daniel Oliveira <oficial.danieloliveira@gmail.com>
2020-12-13 16:07:01 -0300

    feat(express): set up jobs :moneybag:

--- src/Shared/Infra/Http/index.ts
+++ src/Shared/Infra/Http/index.ts
@@ -1,22 +1,25 @@
 import 'dotenv/config';
 import 'reflect-metadata';
 import 'express-async-errors';
+import '../../Containers';
 
 import express from 'express';
 import { errors as celebrateErrors } from 'celebrate';
 
-import '../../Containers';
-
 import TypeORM from '../TypeORM';
+import Jobs from '../Jobs';
 import routes from './Routes/index.routes';
 import HandleErrors from './Middlewares/HandleErrors';
 
 const app = express();
 const port = process.env.APP_PORT || 3333;
 const typeORM = new TypeORM();
+const jobs = new Jobs();
 const handleErrors = new HandleErrors();
 
-typeORM.run();
+typeORM.run().then(() => {
+  jobs.run();
+});
 
 app.use(express.json());
 app.use(routes);
