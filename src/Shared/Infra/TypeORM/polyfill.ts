5bb5fe043f59ae18036f30cb15927d328af8b175
lparolari <luca.parolari23@gmail.com>
2021-09-10 21:28:32 +0200

    chore: patched typeorm for VirtualColumn
    followed Adrian Pietrzak's article at https://pietrzakadrian.com/blog/virtual-column-solutions-for-typeorm

---
+++ src/Shared/Infra/TypeORM/polyfill.ts
@@ -0,0 +1,46 @@
+/* eslint-disable func-names */
+/* eslint-disable no-shadow */
+/* eslint-disable no-param-reassign */
+
+import 'reflect-metadata';
+import { SelectQueryBuilder } from 'typeorm';
+import { VIRTUAL_COLUMN_KEY } from './Decorators/VirtualColumn';
+
+declare module 'typeorm' {
+  interface SelectQueryBuilder<Entity> {
+    getMany(this: SelectQueryBuilder<Entity>): Promise<Entity[] | undefined>;
+    getOne(this: SelectQueryBuilder<Entity>): Promise<Entity | undefined>;
+  }
+}
+
+SelectQueryBuilder.prototype.getMany = async function () {
+  const { entities, raw } = await this.getRawAndEntities();
+
+  const items = entities.map((entitiy, index) => {
+    const metaInfo = Reflect.getMetadata(VIRTUAL_COLUMN_KEY, entitiy) ?? {};
+    const item = raw[index];
+
+    for (const [propertyKey, name] of Object.entries<string>(metaInfo)) {
+      entitiy[propertyKey] = item[name];
+    }
+
+    return entitiy;
+  });
+
+  return [...items];
+};
+
+SelectQueryBuilder.prototype.getOne = async function () {
+  const { entities, raw } = await this.getRawAndEntities();
+  if (!entities || !entities.length) {
+    return undefined;
+  }
+
+  const metaInfo = Reflect.getMetadata(VIRTUAL_COLUMN_KEY, entities[0]) ?? {};
+
+  for (const [propertyKey, name] of Object.entries<string>(metaInfo)) {
+    entities[0][propertyKey] = raw[0][name];
+  }
+
+  return entities[0];
+};
